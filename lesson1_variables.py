6ff27c5deee3044a7174afbf179eba0ecfbf5f6c
Psicotix <davesantorum@googlemail.com>
2021-09-06 10:44:30 +0100

    Extra useful variable info

--- lesson1_variables.py
+++ lesson1_variables.py
@@ -3,7 +3,7 @@ a = 1       # integer
 a1 = 0.1    # float
 b = "1"     # string can be either single or double quotes
 
-# everything else is a function
+# everything else is a function. Or a statement.
 print(a + a1)
 print(b)
 
@@ -37,10 +37,19 @@ print(_fruit)
 print(_seed)
 
 # A tuple is a group of things (variable possibilities). They can be extracted
-# or unpacked after a variable is defined
+# or unpacked after a variable is defined. THIS IS A LIST![] and () is a tuple. I think.
 socks = ["red sock", "green sock", "blue sock"]
 f, g, h = socks
 print(f)
 print(g)
 print(h)
 
+socks = ("red sock", "green sock", "blue sock")
+f, g, h = socks
+print(f)
+print(g)
+print(h)
+# The Print() statement - print statements containing strings and numbers will cause an error.
+print(len(f))   # len shows the length of the string.
+print(f[0])  # prints first character of f string variable - index 0
+print(f[0:2])  # prints the first three characters - index 0 to index 2 - separated by :
