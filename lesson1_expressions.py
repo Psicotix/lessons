9874a29adc1744307e8e14f7adb6940bba63c625
Psicotix <davesantorum@googlemail.com>
2021-09-06 23:29:52 +0100

    Some useful info about expressions.

---
+++ lesson1_expressions.py
@@ -0,0 +1,14 @@
+# An expression consists of values and operators which can always reduce
+# down to a single value.
+print("Operators:")
+print("^^^^^^^^^^")
+print("Exponent")
+print("2 ** 3 =")
+print(2 ** 3)
+print("Modulus or remainder")
+print("21 % 4=")
+print(21 % 4)
+print("Integer division or floored quotient")
+print("21 // 4=")
+print(21 // 4)
+print("And of course division, /, multiplication, *, addition and subtraction")
