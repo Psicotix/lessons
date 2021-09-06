c7bda9a7e836533d9325a9ff406456b5561441eb
Psicotix <davesantorum@googlemail.com>
2021-09-06 12:13:10 +0100

    a test of integer input

---
+++ dogs.py
@@ -0,0 +1,9 @@
+dog = int(input("What is the age of your dog? "))
+# specifying int() with the above variable input prevents just
+# a string being input, allowing the maths below to work.
+if dog > 8:
+    print("The dog's getting old.")
+else:
+    print("The dog's quite young really")
+
+print("Thanks!")
