e74f6d5d4d0c6e7247e3adda1a1ea531ac9820e4
Psicotix <davesantorum@googlemail.com>
2021-09-19 15:40:45 +0100

    Delete newfiletocheck.py
    Checking that it's not deleted from master default branch.

--- newfiletocheck.py
+++
@@ -1,4 +0,0 @@
-# This is a test to check the new SSH key is still working ok.
-print("Hello, Oddbums!")
-# Had to add $git remote set-url origin git@github.com:Psicotix/lessons.git for the new key to work.
-# or maybe it was just the fact that I'd had to rollback.
