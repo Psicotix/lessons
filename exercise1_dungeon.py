61cfe7eca998e7fc9ddf0e83d851fb24f12ad957
Psicotix <davesantorum@googlemail.com>
2021-09-19 23:54:01 +0100

    1/ Better use of the noun - split to list, then remove the "a" or "the" at the start - we'll make it singular in print. 2/ Then use ' '.join() on the list to convert back to a string. 3/ Remove punctuation that testers may add out of relief that something appears to be working on simple yes/no response. 4/ The verb "to ping" was causing problems - it was not possible for the code to suggest "pinging" whatever it was. Fixed in a really ugly way.

--- exercise1_dungeon.py
+++ exercise1_dungeon.py
@@ -6,13 +6,11 @@ choices = []
 # OOOO, WE GOT A FUNCTION!
 # so we need to split the verb_a string to get the actual verb if there's a noun there.
 # fsck it. Use "split". In a function. Which we've not studied yet. Arse.
-
-
-def spit(string):
-    # is it necessary to actually define this function? Can't we just use verb = list(verb_a.split(" ")) later?
-    li = list(string.split(" "))  # so the string will be verb_a
-    return li
-
+# 18 September: NOT NECESSARY!
+# def spit(string):
+# is it necessary to actually define this function? Can't we just use verb = list(verb_a.split(" ")) later?
+# li = list(string.split(" "))  # so the string will be verb_a
+# return li
 
 # so then we go and split verb_a which we extract from choices into verb
 # INTRO:
@@ -31,47 +29,92 @@ What do you use?
 
 item_01 = ["paperclip", "paper clip", "rubber band", "rock", "box of matches", "torch"]
 # INPUT -->
-choices.append(input("> "))
+choices.append(input("> ").lower())
 
 # provide a variable for next line:
 noun_a = choices[0]
+# check if people have "A thing" or "THE thing"...
+noun_a1 = list(noun_a.split(" "))
+# print(noun_a1)
+# print("CHECK THIS ABOVE!")
+if noun_a1[0] == "the":
+    noun_a1.pop(0)
+elif noun_a1[0] == "a":
+    noun_a1.pop(0)
+
+noun_a = ' '.join(noun_a1)
+# print(noun_a, "this is to check noun_a has had THE or A removed.")
 # "test_thing" below is just used for testing the thing. so it's = any(x in our_test_variable for x in big_long_list)
 if any(test_thing in noun_a for test_thing in item_01):
     print("OK!")  # it works!! It lives!!
 else:
     print("Where did you get that from?")  # rhetorical question. "pass" is a better option.
-print(f"You take {noun_a} and hold it up. What do you do with it?")
-choices.append(input("> "))
-verb_a = choices[1]
-verb = spit(verb_a)
-# hopefully using our function on line 11.
-# to do: 1/ should really check if verb includes -ing already. They could be blowing it out.
-if "ing" in verb[0]:
-    print("True")
-    lol = True
+# Did they use the torch?
+if "torch" in noun_a:
+    print("""
+You hold the torch in front of you, and try to switch it on. You hear
+a fizz and a click and for a few seconds you cast a soft light around
+you.
+
+You see reflections from the new light source on the ground, and you seem to be in
+a room that stretches a couple of meters in front of you. There are boxes
+casting shadows, and no windows. 
+
+The torch fades and dies. 
+
+What do you do? 
+""")
+    choices.append(input("> ").lower())
+    # print(choices)  # to see where we are - we are at choices[1]
+    print(f"{choices[1]} doesn't help")
+
 else:
-    lol = False
+    print(f"You take the {noun_a} and hold it up. What do you do with it?")
+choices.append(input("> ").lower())
+verb_a = choices[1]
+# verb = spit(verb_a) # <-- this function is unnecessary, unless we need to do it again, but it's not much
+# code and typing in the command is a better form of learning.
+verb = list(verb_a.split(" "))
+# to do: 1/ should really check if verb includes -ing already. They could be blowing it out.
+if "ping" == verb[0]:
+    ing = False
+elif "ing" in verb[0]:
+    print(verb)
+    print("True")
+    ing = True
+# OMFG - you can't ping something without pinging it.
+
+else:
+    ing = False
 
 # Define positive response in a big list of possible yeses:
 answer_yes = ["yes", "kinda", "a bit", "slightly", "loads", "yeah", "yup", "yus", "si", "really"]
 # we'll use it after the next input. Also handy to have for positive responses later.
 
 print(f"You've got a {noun_a} and you've tried to {verb_a}... Is this helping?")
-helping = input("> ") or "no"
-helping = helping.lower()
+does_it_help = input("> ") or "no"
+does_it_help = does_it_help.lower()
+# punctuation removal:
+punctuation = '''!()-[]{};:'"\, <>./?@#$%^&*_~'''
+helping = ""
+for x in does_it_help:
+    if x not in punctuation:
+        helping = helping + x
+print(helping)
+
 
 # print(verb[0])
-#        2/ should also check if verb ends in e. !FIXED! kind of, with the LOL correction below.
+#        2/ should also check if verb ends in e. !FIXED! kind of, with the ing correction below.
 #        3/ Also should check if "it" is present -- so choices[1] could be something like "put it up your bum"
 # solution to 3 - rephrase line
-if lol:  # so if it's verbing
+if ing:  # so if it's verbing
     if helping in answer_yes:  # so if result of the input for helping is in the list
         print(f"Great! {str(verb[0])} the {noun_a} is helping!")
     else:
         # think we may need to check noun_a for an "a" or "the"
         print(f"OK, so {str(verb[0])} the {noun_a} wasn't such a great idea.")
 
-if not lol:
+if not ing:
     if helping in answer_yes:  # so if result of the input for helping is in the list
         print(f"Great! {str(verb[0])}ing the {noun_a} is helping!")
     else:
@@ -80,4 +123,6 @@ if not lol:
 
 # LET'S END THIS NOW!
 print("Thanks for playing... Goodbye!")
-print(f"BTW, your choices were '{choices[0]}' and '{choices[1]}'")
+print("BTW, your choices were:")
+for n in range(len(choices)):
+    print(choices[n])
